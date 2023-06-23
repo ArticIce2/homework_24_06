f94317edcc9004db0604124e4c582dbf36383189
Artic <artic.blue17@mail.ru>
2023-06-24 00:00:23 +0300

    Homework

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №1
+
+// int Exponentiation (int numberA, int numberB)
+// {
+// int result  = 1;
+// for(int i = 1;  i <= numberB; i++)
+// {
+// result = result * numberA;
+// }
+// return result;
+// }
+// System.Console.WriteLine("Введите число A -> ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число B -> ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+// int exponentiation = Exponentiation(numberA, numberB);
+// Console.WriteLine("Ответ: " + exponentiation);
+
+//Задача 2
+
+// Console.WriteLine("Введите число N -> ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (i > 0)
+// {
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("Cумма  цифр в числе равна: " + sum);
+
+//Задача №3
+
+//Слишком сложная для меня
